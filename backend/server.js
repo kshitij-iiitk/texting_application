d1ece8b2f7adfab786d1088a0f6025b0d48a945a
Kshitij R Phutane <kshitijrphutane.221mn032@nitk.edu.in>
2025-08-04 12:15:53 +0530

    Final Push

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-
-import authRoutes from "./routes/auth.route.js"; 
+import path from "path";
+import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import userRoutes from "./routes/user.route.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
@@ -12,15 +12,29 @@ dotenv.config();
 
 const port = process.env.PORT || 3000;
 
-app.use(express.json()); 
+const __dirname = path.resolve();
+
+app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 app.use("/api/user", userRoutes);
 
+app.use(express.static(path.join(__dirname, "frontend", "dist")));
+
+
+
+
+// IDK whats the problem with this line but its giving pathToRegexpError which i cant resolve 
+
+// app.get("*", (req, res) => {
+// res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+// });
+
+
 
 server.listen(port, () => {
-  connectToMongoDB(); 
+  connectToMongoDB();
   console.log(`Server running at http://localhost:${port}`);
 });
